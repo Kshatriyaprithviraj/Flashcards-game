4463d23d26c8f2db49e427730893c888303695d1
kshatriyaprithviraj <1999prithviraj1999@gmail.com>
2021-06-17 12:07:47 +0530

    card correct and incorrect logic bugs to fix with flash Object recog as undef in app.comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
+import { IFlash } from './flash.model';
 
+function getRandNumbers() {
+  return Math.floor(Math.random() * 100000);
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flashcards-game';
+  title = 'Flashcards-game';
+  editing = false;
+  editingId: number | undefined;
+
+  flashs: IFlash[] = [
+    {
+      question: 'Question 1',
+      answer: 'Answer 1',
+      show: false,
+      id: getRandNumbers(),
+    },
+    {
+      question: 'Question 2',
+      answer: 'Answer 2',
+      show: false,
+      id: getRandNumbers(),
+    },
+    {
+      question: 'Question 3',
+      answer: 'Answer 3',
+      show: false,
+      id: getRandNumbers(),
+    },
+    {
+      question: 'Question 4',
+      answer: 'Answer 4',
+      show: false,
+      id: getRandNumbers(),
+    },
+    {
+      question: 'Question 5',
+      answer: 'Answer 5',
+      show: false,
+      id: getRandNumbers(),
+    },
+  ];
+
+  trackByFlashId(index: any, flash: { id: any; }) {
+    return flash.id;
+  }
+
+  handleToggleCard(id: number) {
+    const flash = this.flashs.find(flash => flash.id === id);
+    flash.show = !flash.show;
+  }
+
+  handleDelete(id: number) {
+    const flashId = this.flashs.indexOf(flash => flash.id === id);
+    this.flashs.splice(flashId, 1);
+  }
+
+  handleEdit(id: number) {
+    this.editing = true;
+    this.editingId = id;
+  }
+
+  handleRememberedChange({id, flag}) {
+    const flash = this.flashs.find(flash => flash.id === id);
+    flash.remembered = flag;
+  }
 }
--- src/app/flash/flash.component.ts
+++ src/app/flash/flash.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {IFlash} from './../flash/flash.model';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {IFlash} from '../flash.model';
 
 @Component({
   selector: 'app-flash',
@@ -12,9 +12,36 @@ export class FlashComponent implements OnInit {
     id: 1,
     question: 'React to Angular',
     answer: 'No React-ion',
-    show: 'false'
+    show: false
   }
 
+  @Output() onToggleCard = new EventEmitter();
+  toggleCard() {
+    this.onToggleCard.emit(this.flash.id);
+  }
+
+  @Output() onDelete = new EventEmitter();
+  @Output() onEdit = new EventEmitter();
+  @Output() onRememberedChange = new EventEmitter();
+
+  deleteFlash() {
+    this.onDelete.emit(this.flash.id);
+  }
+  editFlash() {
+    this.onEdit.emit(this.flash.id);
+  }
+  markCorrect() {
+    this.onRememberedChange.emit({
+      id: this.flash.id,
+      flag: 'correct',
+    });
+  }
+  markIncorrect() {
+    this.onRememberedChange.emit({
+      id: this.flash.id,
+      flag: 'incorrect',
+    });
+  }
   constructor() { }
 
   ngOnInit(): void {
