2ef05c34d089f13b571ac86ee1e9c07b159d4b05
kshatriyaprithviraj <1999prithviraj1999@gmail.com>
2021-06-21 20:56:16 +0530

    still tonnes of bugs to fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlashComponent } from './flash/flash.component';
+import { FlashService } from './flash.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { FlashComponent } from './flash/flash.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [FlashService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
